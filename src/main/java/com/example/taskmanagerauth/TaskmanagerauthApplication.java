f4bae0e3cf5880acee0da6a2a39f27c1dcbed661
Auwate <auwate1@gmail.com>
2025-02-20 12:10:36 -0500

    first commit

---
+++ src/main/java/com/example/taskmanagerauth/TaskmanagerauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskmanagerauth/TaskmanagerauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagerauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
