0294c67add1b9a2dc6068a0cd95dfc7933db1e86
Auwate <auwate1@gmail.com>
2025-03-24 11:19:28 -0400

    Commit:
    # Description:
    - Tackles issue #15

--- ssl_build.sh
+++ ssl_build.sh
@@ -1,16 +1,5 @@
 #!/bin/env bash
 
-CERT_DETAILS=$(cat <<EOF
-US
-FL
-Miami
-MyCompany
-MyDepartment
-springboot.example.com
-.
-.
-EOF
-)
-
-openssl req -newkey rsa:2048 -nodes -keyout springboot.pem -x509 -days 365 -out certificate.pem <<< "$CERT_DETAILS"
+openssl req -newkey rsa:2048 -nodes -keyout springboot.pem -x509 -days 365 -out certificate.pem -subj "/C=US/ST=FL/L=Miami/O=MyCompany/OU=MyDepartment/CN=springboot.example.com"
 openssl pkcs12 -export -in certificate.pem -inkey springboot.pem -out springboot.p12 -name springboot -password pass:admin
+keytool -import -trustcacerts -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -noprompt -alias springboot -file certificate.pem
