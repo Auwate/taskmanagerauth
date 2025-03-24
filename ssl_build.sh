942c8a3b8f1b091c9cdd7fcc454e72081ad6475e
Auwate <auwate1@gmail.com>
2025-03-24 11:09:07 -0400

    Commit:
    # Description:
    - Tackles issue #15

--- ssl_build.sh
+++ ssl_build.sh
@@ -13,4 +13,4 @@ EOF
 )
 
 openssl req -newkey rsa:2048 -nodes -keyout springboot.pem -x509 -days 365 -out certificate.pem <<< "$CERT_DETAILS"
-openssl pkcs12 -export -in certificate.pem -inkey springboot.pem -out springboot.p12 -name myapp -password pass:admin
+openssl pkcs12 -export -in certificate.pem -inkey springboot.pem -out springboot.p12 -name springboot -password pass:admin
