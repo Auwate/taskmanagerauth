b8b86f530266930e6cce5f381bd3f060dfba9d00
Auwate <auwate1@gmail.com>
2025-03-24 11:25:56 -0400

    Commit:
    # Description:
    - Tackles issue #15

--- ssl_build.sh
+++ ssl_build.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
-openssl req -newkey rsa:2048 -nodes -keyout springboot.pem -x509 -days 365 -out certificate.pem -subj "/C=US/ST=FL/L=Miami/O=MyCompany/OU=MyDepartment/CN=springboot.example.com"
-openssl pkcs12 -export -in certificate.pem -inkey springboot.pem -out springboot.p12 -name springboot -password pass:admin
-keytool -import -trustcacerts -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -noprompt -alias springboot -file certificate.pem
+openssl genpkey -algorithm RSA -out ca.key
+openssl req -new -x509 -key ca.key -out ca.crt -days 3650 -subj "/C=US/ST=FL/L=Miami/O=MyCompany/OU=MyDepartment/CN=springboot.example.com"
+openssl genpkey -algorithm RSA -out springboot.key
+openssl req -new -key springboot.key -out springboot.csr -subj "/C=US/ST=FL/L=Miami/O=MyCompany/OU=MyDepartment/CN=springboot.example.com"
+openssl x509 -req -in springboot.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out springboot.crt -days 365
+openssl pkcs12 -export -in springboot.crt -inkey springboot.key -out springboot.p12 -name springboot -CAfile ca.crt -caname rootCA -passout pass:admin
